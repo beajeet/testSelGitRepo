a089ee776040b88d27b9eaf03cb522fc6ba55c94
ajeetpratapsingh <ajeetpratapsingh@192.168.225.150>
2020-04-03 21:26:17 +0530

    initial commit

---
+++ MyMavenProjectName/src/test/java/MyMavenGroupID/MyMavenProjectName/UniqueTest.java
@@ -0,0 +1,13 @@
+package MyMavenGroupID.MyMavenProjectName;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class UniqueTest {
+	
+@Test
+public void joker() {
+	System.out.println("Test print here ...");
+}
+	
+}
